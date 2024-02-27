215cb1bb160d346ada4e700634be1e834f0f4022
Santiago Martín Agra <santiagomartinagra@gmail.com>
2024-02-27 21:03:12 +0100

    lint: apply linting rules

--- test/prices.spec.ts
+++ test/prices.spec.ts
@@ -1,26 +1,22 @@
-import {assert, expect} from 'chai';
 import request from 'supertest-as-promised';
-import {createApp} from "../src/prices"
+
+import { createApp } from '../src/prices';
 
 describe('prices', () => {
+	let app, connection;
 
-    let app, connection
+	beforeEach(async () => {
+		({ app, connection } = await createApp());
+	});
 
-    beforeEach(async () => {
-        ({app, connection} = await createApp());
-    });
+	afterEach(async () => {
+		await connection.end();
+	});
 
-    afterEach(async () => {
-        await connection.end()
-    });
+	it('does something', async () => {
+		const response = await request(app).get('/prices?type=1jour');
 
-    it('does something', async () => {
-
-        const response = await request(app)
-            .get('/prices?type=1jour')
-
-        var expectedResult = {cost: 35} // change this to make the test pass
-        expect(response.body).deep.equal(expectedResult)
-    });
-
+		const expectedResult = { cost: 35 }; // change this to make the test pass
+		expect(response.body).toEqual(expectedResult);
+	});
 });
